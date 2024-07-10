5a72e05075f07d121f8af570088a1c8fcf419f83
lyssautida <lyssamailbox@gmail.com>
2024-07-10 18:24:38 -0400

    chore: database config

--- agro-backend/Program.cs
+++ agro-backend/Program.cs
@@ -1,7 +1,13 @@
+using agro_backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+var connectionString = builder.Configuration.GetConnectionString("AgroDataBase");
+
+// Add services to the container.
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<AgroDBContext>(options => options.UseNpgsql(connectionString));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
